dabd2821f0c548927f1b9fd280c108e681fcdef8
Edvinas Pranka <epranka@gmail.com>
2019-11-26 16:03:18 +0200

    improvement(templates): added build instructions in README template
    #15

--- utils/createREADME.js
+++ utils/createREADME.js
@@ -35,6 +35,23 @@ ${authorBlock.join("\n\n")}
 `
     : ""
 }
+## Build
+
+\`\`\`
+npm run build // for single build
+
+npm run watch // to watch changes
+\`\`\`
+
+or
+
+\`\`\`
+yarn build // for single build
+
+yarn watch // to watch changes
+\`\`\`
+
+
 ## License
 
 ${licenseContent}
