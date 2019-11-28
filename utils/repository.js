a822eb2f901420318d41c3a2bfd54bb34a6fdf9f
Edvinas Pranka <epranka@gmail.com>
2019-11-28 11:52:23 +0200

    fix: fixed bug when trying to create new package without git
    Fixed bug when trying to create new package without git
    
    Closes #6

--- utils/repository.js
+++ utils/repository.js
@@ -1,24 +1,28 @@
 const fs = require("fs");
 
 const getRepositoryURL = () => {
-  let gitconf = fs.readFileSync(".git/config", "utf-8");
-  if (!gitconf) return "";
-  gitconf = gitconf.split(/\r?\n/);
-  const i = gitconf.indexOf('[remote "origin"]');
-  let u;
-  if (i !== -1) {
-    u = gitconf[i + 1];
-    if (!u.match(/^\s*url =/)) u = gitconf[i + 2];
-    if (!u.match(/^\s*url =/)) u = null;
-    else u = u.replace(/^\s*url = /, "");
-  }
-  if (u && u.match(/^git@github.com:/)) {
-    u = u.replace(/^git@github.com:/, "https://github.com/");
-  }
-  if (!u) {
+  try {
+    let gitconf = fs.readFileSync(".git/config", "utf-8");
+    if (!gitconf) return "";
+    gitconf = gitconf.split(/\r?\n/);
+    const i = gitconf.indexOf('[remote "origin"]');
+    let u;
+    if (i !== -1) {
+      u = gitconf[i + 1];
+      if (!u.match(/^\s*url =/)) u = gitconf[i + 2];
+      if (!u.match(/^\s*url =/)) u = null;
+      else u = u.replace(/^\s*url = /, "");
+    }
+    if (u && u.match(/^git@github.com:/)) {
+      u = u.replace(/^git@github.com:/, "https://github.com/");
+    }
+    if (!u) {
+      return "";
+    }
+    return u;
+  } catch (_) {
     return "";
   }
-  return u;
 };
 
 module.exports = getRepositoryURL;
